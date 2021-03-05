7f929604f1c176c2e147a8cc32b1a4efd09aaa27
Nicola <lanzoni.nicola@gmail.com>
2021-03-05 00:58:54 -0300

    update useResponsiveSize function

--- src/styling/_hooks/useResponsiveSize.js
+++ src/styling/_hooks/useResponsiveSize.js
@@ -1,6 +1,7 @@
 import { composeCSSValue, breakpointsDesignSpec } from "@layouts/lib/index"
 import { useViewportInfo } from "@hooks"
 import { isObject, isNumber, isString, log } from "@utils/index"
+import React from "react"
 
 // SOURCE
 // https://blog.typekit.com/2016/08/17/flexible-typography-with-css-locks/
@@ -13,6 +14,7 @@ export default function useResponsiveSize(
 ) {
   // I can't use ThemeProvider data, some components using this function are mounted
   // before the values requested here (device, diagonal) and located in the "theme" are available
+  const [resultSize, setResultSize] = React.useState(`${size}px`)
   const {
     device: currentDeviceFormFactor,
     diagonal: currentViewportDiagonal,
@@ -21,56 +23,61 @@ export default function useResponsiveSize(
     currentDeviceFormFactor
   )
 
-  let userSize = 0
-  let userUnit = null
-  let resultSize = 0
+  React.useEffect(() => {
+    let userSize = 0
+    let userUnit = null
+    let _resultSize = 0
 
-  const isFixedSize = isNumber(size) || isString(size)
-  const isResponsiveSize = isObject(size)
+    const isFixedSize = isNumber(size) || isString(size)
+    const isResponsiveSize = isObject(size)
 
-  if (isFixedSize) {
-    const { value, unit } = composeCSSValue(size)
-    userSize = value
-    userUnit = unit
-  }
+    if (isFixedSize) {
+      const { value, unit } = composeCSSValue(size)
+      userSize = value
+      userUnit = unit
+    }
 
-  if (isResponsiveSize) {
-    const responsiveSize = size[currentDeviceFormFactor] ?? 0
-    const { value, unit } = composeCSSValue(responsiveSize)
-    userSize = value
-    userUnit = unit
-  }
+    if (isResponsiveSize) {
+      const responsiveSize = size[currentDeviceFormFactor] ?? 0
+      const { value, unit } = composeCSSValue(responsiveSize)
+      userSize = value
+      userUnit = unit
+    }
 
-  if (userUnit === "px" || userUnit === "PX") {
-    resultSize = Math.round(
-      (currentViewportDiagonal / viewportDiagonalDesignSpec) * userSize
-    )
-  } else {
-    resultSize = userSize
-  }
+    if (userUnit === "px" || userUnit === "PX") {
+      _resultSize = Math.round(
+        (currentViewportDiagonal / viewportDiagonalDesignSpec) * userSize
+      )
+    } else {
+      _resultSize = userSize
+    }
 
-  console.log(
-    "useresponsivesize currentDeviceFormFactor",
-    currentDeviceFormFactor,
-    "currentViewportDiagonal",
-    currentViewportDiagonal,
-    "viewportDiagonalDesignSpec",
-    viewportDiagonalDesignSpec,
-    "userSize",
-    userSize,
-    "resultSize",
-    resultSize
-  )
+    setResultSize(() => setResultSize(`${_resultSize}${userUnit}`))
+  })
 
-  if (debug) {
-    log("useResponsiveSize", {
-      currentViewportDiagonal,
-      viewportDiagonalDesignSpec,
-      userSize,
-      userUnit,
-      resultSize,
-    })
-  }
+  // console.log(
+  //   "useresponsivesize currentDeviceFormFactor",
+  //   currentDeviceFormFactor,
+  //   "currentViewportDiagonal",
+  //   currentViewportDiagonal,
+  //   "viewportDiagonalDesignSpec",
+  //   viewportDiagonalDesignSpec,
+  //   "userSize",
+  //   userSize,
+  //   "resultSize",
+  //   resultSize
+  // )
 
-  return `${resultSize}${userUnit}`
+  // if (debug) {
+  //   log("useResponsiveSize", {
+  //     currentViewportDiagonal,
+  //     viewportDiagonalDesignSpec,
+  //     userSize,
+  //     userUnit,
+  //     resultSize,
+  //   })
+  // }
+
+  // return `${resultSize}${userUnit}`
+  return resultSize
 }
